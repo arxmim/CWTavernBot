ddab33b5fc359e71b9e3cf4dde8408206247e514
arxmim <ivannaz@yandex.ru>
2017-05-22 17:33:46 +0300

    tokens

--- src/main/java/org/nia/PropertiesLoader.java
+++ src/main/java/org/nia/PropertiesLoader.java
@@ -15,14 +15,14 @@ public class PropertiesLoader {
     }
 
     public String getBotToken() {
-        return "340515593:AAGnZloyTSkKs90p8_FPdYoL-wVOFA41SvA";
-//        return System.getenv("botToken");
+//        return "340515593:AAGnZloyTSkKs90p8_FPdYoL-wVOFA41SvA";
+        return System.getenv("botToken");
     }
 
     public String getConnectionString() {
-        return "jdbc:postgresql://ec2-54-247-99-159.eu-west-1.compute.amazonaws.com:5432/" +
-                        "dc2mdupck03c34?user=hltzjztvfqatid&password=" +
-                        "763351c1f0d1758070a3a9fcee498ddd61cf1e10c8826a0f49ad8f4066f18f25&sslmode=require&PORT=32766";
-//        return System.getenv("JDBC_DATABASE_URL");
+//        return "jdbc:postgresql://ec2-54-247-99-159.eu-west-1.compute.amazonaws.com:5432/" +
+//                        "dc2mdupck03c34?user=hltzjztvfqatid&password=" +
+//                        "763351c1f0d1758070a3a9fcee498ddd61cf1e10c8826a0f49ad8f4066f18f25&sslmode=require&PORT=32766";
+        return System.getenv("JDBC_DATABASE_URL");
     }
 }
